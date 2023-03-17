a56c161aba752b9c4f5b553c282cb6a528a485d3
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2023-03-17 13:57:08 -0400

    chore(root): sync

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head';
-import { Button, Card } from 'ui';
+import Head from "next/head"
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Web - Turborepo Example</title>
+        <title>Web</title>
       </Head>
 
-      <main className='mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8'>
-        <div className='mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8'>
-          <Button />
+      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+          Hello
         </div>
       </main>
     </div>
-  );
+  )
 }
